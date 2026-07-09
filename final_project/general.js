fb0109b2c7a9b791fd28e6aad478f8d51ab205c0
Sisanda Madikizela <165172030+sisanda2504@users.noreply.github.com>
2026-07-09 15:03:04 +0200

    Refactor API calls to use a single URL variable

--- final_project/general.js
+++ final_project/general.js
@@ -1,90 +1,62 @@
 const axios = require("axios");
 
-const BASE_URL = "http://localhost:5000";
+const url = "http://localhost:5000";
 
 
 async function getAllBooks() {
     try {
-        const response = await axios.get(`${BASE_URL}/`);
-
-        if (response.status === 200) {
-            return response.data;
-        }
-
-        return "Unable to retrieve books";
-
-    } catch (error) {
-        return error.response
-            ? error.response.data
-            : error.message;
+        let response = await axios.get(url);
+        console.log(response.data);
+    }
+    catch(error) {
+        console.log(error.message);
     }
 }
 
 
 async function getBooksByISBN(isbn) {
     try {
-        const response = await axios.get(
-            `${BASE_URL}/isbn/${isbn}`
-        );
-
-        if (response.status === 200 && response.data) {
-            return response.data;
-        }
-
-        return "Book not found";
-
-    } catch (error) {
-        return error.response
-            ? error.response.data
-            : error.message;
+        let response = await axios.get(`${url}/isbn/${isbn}`);
+        console.log(response.data);
+    }
+    catch(error) {
+        console.log(error.message);
     }
 }
 
 
 async function getBooksByAuthor(author) {
     try {
-        const response = await axios.get(
-            `${BASE_URL}/author/${encodeURIComponent(author)}`
+        let response = await axios.get(
+            `${url}/author/${author}`
         );
 
-        if (
-            response.status === 200 &&
-            Object.keys(response.data).length > 0
-        ) {
-            return response.data;
-        }
-
-        return "No books found for author";
-
-    } catch (error) {
-        return error.response
-            ? error.response.data
-            : error.message;
+        console.log(response.data);
+    }
+    catch(error) {
+        console.log(error.message);
     }
 }
 
 
 async function getBooksByTitle(title) {
     try {
-        const response = await axios.get(
-            `${BASE_URL}/title/${encodeURIComponent(title)}`
+        let response = await axios.get(
+            `${url}/title/${title}`
         );
 
-        if (
-            response.status === 200 &&
-            Object.keys(response.data).length > 0
-        ) {
-            return response.data;
-        }
-
-        return "No books found for title";
-
-    } catch (error) {
-        return error.response
-            ? error.response.data
-            : error.message;
+        console.log(response.data);
+    }
+    catch(error) {
+        console.log(error.message);
     }
 }
+
+
+getAllBooks();
+getBooksByISBN(1);
+getBooksByAuthor("Chinua Achebe");
+getBooksByTitle("Things Fall Apart");
 
 
 module.exports = {
